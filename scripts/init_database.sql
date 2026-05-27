fdcd38ae20e2db176f53695f8b95a35a757f083a
imRJha19 <2023uce0041@iitjammu.ac.in>
2026-05-27 18:40:32 +0530

    Update init_database.sql with cautionary notes
    Added warnings about dropping the database and schemas.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -3,6 +3,11 @@
    Creates DataWarehouse database and Medallion schemas
 
 this scripts will generate a database named datawarehouse if its already exits it will drop it so take care pf past database 
+this script sets up three schemas within the database : 'Bronze' ,'Silver' , 'Gold'.
+
+WARNING - running this scripts will drop the entire 'DATAWAREHOUSE' database if it exists , 
+all data in the database will be permanently deleted . proceed with caution and ensure 
+you have proper backup before running this scripts 
    ========================================================= */
 
 USE master;
